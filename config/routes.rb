2a8afecf42589f043ffe22249e3c559878a5c57e
yoshiky <y.nagatsuka0524@gmail.com>
2016-04-25 01:13:26 +0900

    API通信用のstub追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :monsters, except: :show
   resources :dungeons, except: :show
   resources :dungeon_histories, except: :show
+
+  namespace :api, { format: 'json' }do
+    resources :gacha_histories, only: :create
+  end
+  namespace :stub, { format: 'json' }do
+    resources :gacha_histories, only: :create
+  end
 end
